581787eaf62f3bc6aaf0365a94209a215a616810
R-e-i-s <rafael.creis@sptech.school>
2022-05-16 23:00:35 -0300

    sobrenos finalizado

---
+++ src/functions.js
@@ -0,0 +1,56 @@
+function pergunta1() {
+  document.getElementById("apresentacao").style.display = "none";
+  document.getElementById("pergunta1").style.display = "flex";
+  document
+    .getElementById("borda_especial")
+    .scrollIntoView({ behavior: "auto" });
+}
+function pergunta2() {
+  document.getElementById("pergunta1").style.display = "none";
+  document.getElementById("pergunta2").style.display = "flex";
+  document
+    .getElementById("borda_especial")
+    .scrollIntoView({ behavior: "auto" });
+}
+function pergunta3() {
+  document.getElementById("pergunta2").style.display = "none";
+  document.getElementById("pergunta3").style.display = "flex";
+  document
+    .getElementById("borda_especial")
+    .scrollIntoView({ behavior: "auto" });
+}
+function pergunta4() {
+    document.getElementById("pergunta3").style.display = "none"
+    document.getElementById("pergunta4").style.display = "flex"
+    document
+    .getElementById("borda_especial")
+    .scrollIntoView({ behavior: "auto" });
+}
+function pergunta5() {
+    document.getElementById("pergunta4").style.display = "none"
+    document.getElementById("pergunta5").style.display = "flex"
+    document
+    .getElementById("borda_especial")
+    .scrollIntoView({ behavior: "auto" });
+}
+function pergunta6() {
+    document.getElementById("pergunta5").style.display = "none"
+    document.getElementById("pergunta6").style.display = "flex"
+    document
+    .getElementById("borda_especial")
+    .scrollIntoView({ behavior: "auto" });
+}
+function pergunta7() {
+    document.getElementById("pergunta6").style.display = "none"
+    document.getElementById("pergunta7").style.display = "flex"
+    document
+    .getElementById("borda_especial")
+    .scrollIntoView({ behavior: "auto" });
+}
+function final() {
+    document.getElementById("pergunta7").style.display = "none"
+    document.getElementById("final").style.display = "flex"
+    document
+    .getElementById("borda_especial")
+    .scrollIntoView({ behavior: "auto" });
+}
